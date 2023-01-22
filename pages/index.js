3ec533e56b340898faf8ed8c0be6a30614540530
B8N4S8N <79737258+B8N4S8N@users.noreply.github.com>
2023-01-22 09:12:21 +0000

    new links / logo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import buildspaceLogo from '../assets/Frame 29.png';
 import { useState } from 'react';
 
 const Home = () => {
@@ -84,13 +84,13 @@ const Home = () => {
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-writer"
+          href="http://opensourcerecovery.life"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p></p>
           </div>
         </a>
       </div>
