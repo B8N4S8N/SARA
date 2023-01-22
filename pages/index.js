14cdba05c6189ec7ff05d52ffbb534a593ce7d9d
B8N4S8N <79737258+B8N4S8N@users.noreply.github.com>
2023-01-22 17:31:47 +0000

    new title

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ const Home = () => {
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Own Your Path w/S.A.R.A.</title>
       </Head>
       <div className="container">
         <div className="header">
