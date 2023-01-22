a7ffd465a782c7054df2be1734c5ff85dd28840b
B8N4S8N <79737258+B8N4S8N@users.noreply.github.com>
2023-01-22 18:57:19 +0000

    change header

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Open Source Recovery</h1>
+            <h1>Serenium's AI Recovery Assistant</h1>
             
           </div>
           <div className="header-subtitle">
