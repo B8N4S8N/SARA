d964c47d5fee1167baca23fde442bcc9c161f033
B8N4S8N <79737258+B8N4S8N@users.noreply.github.com>
2023-01-22 09:12:07 -0800

    Update _document.js
    update twitter card, bar title

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Own Your Path" key="title"/>
+        <meta property="og:description" content="Chat with SARA from O.S.R." key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://res.cloudinary.com/guttermade-llc/image/upload/v1674407484/Frame_29_cprrhl.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
