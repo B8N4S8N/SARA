5f5e158aebfe4b99f9a025792ff3896dd35a09d4
B8N4S8N <79737258+B8N4S8N@users.noreply.github.com>
2023-01-25 02:55:51 +0000

    add analytics and prompt

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import './styles.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <><Component {...pageProps} /><Analytics /></>  
 }
 export default App;
